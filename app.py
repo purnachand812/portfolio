ffd5ab033170c1aac6c1cd1b3413b9b9a14a4a99
purnachand dandu <50629828+purnachand812@users.noreply.github.com>
2023-02-24 22:39:43 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def load_lottieurl(url):
 def local_css(file_name):
         with open(file_name) as f:
              st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
-local_css("style.css")
+local_css("style/style.css")
 
 
 
